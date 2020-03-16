c4299fc04afa17251d01e82d080a99ff72c186f4
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2020-03-16 14:17:23 -0400

    Base Commit

---
+++ Velocity Shooter/Assets/Scripts/TankController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

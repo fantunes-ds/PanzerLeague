b200057d01053b47967113d614c084ac6792e64d
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2020-03-17 09:55:42 -0400

    Tank Controller first try

--- Velocity Shooter/Assets/Scripts/TankController.cs
+++ Velocity Shooter/Assets/Scripts/TankController.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TankController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject canon;
+    private GameObject m_canon;
+    [SerializeField]
+    private Camera m_tankCamera;
 
+    [SerializeField]
+    private float m_tankSpeed = 3;
+    [SerializeField]
+    private float m_rotSpeed = 30;
+    
+    private Rigidbody m_rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+        Vector3 newPos = transform.position + (transform.forward * Input.GetAxis("Vertical") * m_tankSpeed * Time.deltaTime);  
         
+        Quaternion newRot = transform.rotation * Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * m_rotSpeed * Time.deltaTime); 
+
+        newRot.z = 0;
+        m_rb.MovePosition(newPos);
+        m_rb.MoveRotation(newRot);
     }
 }

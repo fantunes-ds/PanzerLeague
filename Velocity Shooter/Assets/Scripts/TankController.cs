13189e44f6163aeb81f80a23b86fbd412464af8e
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2020-03-17 11:04:35 -0400

    Canon rotation

--- Velocity Shooter/Assets/Scripts/TankController.cs
+++ Velocity Shooter/Assets/Scripts/TankController.cs
@@ -16,22 +16,27 @@ public class TankController : MonoBehaviour
     private float m_rotSpeed = 30;
     
     private Rigidbody m_rb;
+    private Rigidbody m_canonRb;
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
+        m_canonRb = m_canon.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.Log(Input.GetAxis("Horizontal2"));
 
         Vector3 newPos = transform.position + (transform.forward * Input.GetAxis("Vertical") * m_tankSpeed * Time.deltaTime);  
         
         Quaternion newRot = transform.rotation * Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * m_rotSpeed * Time.deltaTime); 
+        Quaternion canonRot = m_canon.transform.rotation * Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal2") * m_rotSpeed * Time.deltaTime); 
 
         newRot.z = 0;
         m_rb.MovePosition(newPos);
         m_rb.MoveRotation(newRot);
+        m_canonRb.MoveRotation(canonRot);
     }
 }

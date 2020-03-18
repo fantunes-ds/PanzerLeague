310359b7f1f5ec58fa9510b7f46bc5bb2d2fcd9c
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2020-03-18 14:53:45 -0400

    Fixed Canon rotation rolling

--- Velocity Shooter/Assets/Scripts/TankController.cs
+++ Velocity Shooter/Assets/Scripts/TankController.cs
@@ -20,34 +20,49 @@ public class TankController : MonoBehaviour
     private float m_canonRotSpeed = 30;
 
     private Rigidbody m_rb;
-    private Rigidbody m_canonRb;
+    private GameObject m_canonRb;
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
-        m_canonRb = m_canon.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Quaternion tankRotation = transform.rotation * Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * m_tankRotSpeed * Time.deltaTime);
+        ControlTank();
+        RotateTank();
+        RotateCanon();
+    }
 
-        Vector3 canonRotationAxis = Vector3.up * Input.GetAxis("Horizontal2") + Vector3.right * Input.GetAxis("Vertical2");
-        
-        Quaternion canonRotation = Quaternion.Euler(canonRotationAxis) *  m_canon.transform.rotation; 
+    void RotateCanon()
+    {
+        Vector3 canonRotationAxis = Vector3.right * Input.GetAxis("Vertical2") + Vector3.up * Input.GetAxis("Horizontal2");
+        Quaternion canonRotation = m_canon.transform.rotation * Quaternion.Euler(canonRotationAxis); 
 
+        //Clamp X rotation
         if (canonRotation.eulerAngles.x > 10.0f && canonRotation.eulerAngles.x < 270.0f)
             canonRotation = Quaternion.Euler(9.0f, canonRotation.eulerAngles.y, canonRotation.eulerAngles.z);
         else if (canonRotation.eulerAngles.x < 350.0f && canonRotation.eulerAngles.x > 270.0f )
             canonRotation = Quaternion.Euler(350.0f, canonRotation.eulerAngles.y, canonRotation.eulerAngles.z);
-
-        canonRotation.z = 0;
         
+        
+
+        
+        Quaternion q = canonRotation;
+        q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0);
+        m_canon.transform.rotation = q;
+    }
+
+    void ControlTank()
+    {
         if (Mathf.Abs(m_rb.velocity.x) + Mathf.Abs(m_rb.velocity.y) + Mathf.Abs(m_rb.velocity.z) < m_maxSpeed)
         {m_rb.AddForce(transform.forward * Input.GetAxis("Trigger") * m_tankSpeed);}
-        
+    }
+
+    void RotateTank()
+    {
+        Quaternion tankRotation = transform.rotation * Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * m_tankRotSpeed * Time.deltaTime);
         m_rb.MoveRotation(tankRotation);
-        m_canonRb.MoveRotation(canonRotation);
     }
 }
